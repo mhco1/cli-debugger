33344d36b2662152ff6884d79a6589039ed691d4
MatheusHenrique2000 <matheus.henrique.cortez.oliveira@gmail.com>
2024-01-13 16:24:32 -0300

    add input component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Text } from 'ink';
+// import { useState, useEffect, createContext } from 'react';
+import { Box, Text } from 'ink';
+// import Start from '~/components/start.js';
+// import Repl from '~/components/repl.js';
+import Input from '~/src/components/input.js';
 
 export default ({ }) => <>
-    <Text>App test</Text>
+    <Box flexDirection="column">
+        <Input />
+    </Box>
 </>
---
+++ src/components/input.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Box, Text, useInput } from 'ink';
+
+const insertString = (t, tt, i = 0) => t.slice(0, i) + tt + t.slice(i);
+const removeString = (t, i = 0) => t.slice(0, i - 1) + t.slice(i);
+const splitString = (t, i = 0) => [t.slice(0, i), t.slice(i)];
+
+const Cursor = ({ k }) => {
+
+    return <>
+        <Text inverse={typeof type === 'undefined'}>{k == '' ? ' ' : k}</Text>
+    </>
+}
+
+export default ({ }) => {
+    const [hist, setHist] = useState([]);
+    const [s, setS] = useState({
+        txt: '',
+        pos: 0,
+    });
+
+    const [txt1, _txt2] = splitString(s.txt, s.pos);
+    const [k, txt2] = splitString(_txt2, 1);
+
+    useInput((input, key) => {
+        if (key.leftArrow) return (s.pos > 0 && setS(prev => ({ ...prev, pos: prev.pos - 1 })));
+        if (key.rightArrow) return (s.pos < s.txt.length && setS(prev => ({ ...prev, pos: prev.pos + 1 })));
+        if (key.delete) return (s.pos > 0 && setS(prev => ({
+            ...prev,
+            txt: removeString(prev.txt, prev.pos),
+            pos: prev.pos - 1,
+        })));
+
+        setS(prev => ({
+            ...prev,
+            txt: insertString(prev.txt, input, prev.pos),
+            pos: prev.pos + 1,
+        }));
+    })
+
+    return <>
+        <Text>{txt1}<Cursor k={k} />{txt2}</Text>
+    </>
+}
