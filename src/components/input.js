589403ec0014dcf8b599498cc4094da706f0033b
MatheusHenrique2000 <matheus.henrique.cortez.oliveira@gmail.com>
2024-01-14 23:44:11 -0300

    feature add history to input component

--- src/components/input.js
+++ src/components/input.js
@@ -12,32 +12,47 @@ const Cursor = ({ k }) => {
     </>
 }
 
-export default ({ onSubmit }) => {
-    const [hist, setHist] = useState([]);
+export default ({ onSubmit, history }) => {
     const [s, setS] = useState({
         txt: '',
         pos: 0,
+        hist: Array.isArray(history) ? history : [],
+        histPos: 0,
     });
 
     const [txt1, _txt2] = splitString(s.txt, s.pos);
     const [k, txt2] = splitString(_txt2, 1);
 
     useInput((input, key) => {
-        if (key.upArrow) return
-        if (key.downArrow) return
-        if (key.leftArrow) return (s.pos > 0 && setS(prev => ({ ...prev, pos: prev.pos - 1 })));
-        if (key.rightArrow) return (s.pos < s.txt.length && setS(prev => ({ ...prev, pos: prev.pos + 1 })));
-        if (key.return) return onSubmit(s.txt);
-        if (key.delete) return (s.pos > 0 && setS(prev => ({
-            ...prev,
-            txt: removeString(prev.txt, prev.pos),
-            pos: prev.pos - 1,
+
+        if (key.upArrow) return (s.histPos < s.hist.length - 1 && setS(s =>
+            ({ ...s, txt: s.hist[s.histPos + 1], histPos: s.histPos + 1 })
+        ));
+
+        if (key.downArrow) return (s.histPos > 0 && setS(s =>
+            ({ ...s, txt: [s.txt, ...s.hist][s.histPos - 1], histPos: s.histPos - 1 })
+        ));
+
+        if (key.leftArrow) return (s.pos > 0 && setS(s =>
+            ({ ...s, pos: s.pos - 1 }))
+        );
+
+        if (key.rightArrow) return (s.pos < s.txt.length && setS((s) =>
+            ({ ...s, pos: s.pos + 1 }))
+        );
+
+        if (key.return) {
+            onSubmit(s.txt); return setS(s =>
+                ({ ...s, txt: '', pos: 0, hist: [s.txt, ...s.hist] })
+            )
+        };
+
+        if (key.delete) return (pos > 0 && setS(s => ({
+            ...s, txt: removeString(s.txt, s.pos), pos: s.pos - 1,
         })));
 
-        setS(prev => ({
-            ...prev,
-            txt: insertString(prev.txt, input, prev.pos),
-            pos: prev.pos + 1,
+        setS(s => ({
+            ...s, txt: insertString(s.txt, input, s.pos), pos: s.pos + 1, histPos: 0
         }));
     })
 
