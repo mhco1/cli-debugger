4992b9b2a8975520e955e54e4f2de425c33e3f29
MatheusHenrique2000 <matheus.henrique.cortez.oliveira@gmail.com>
2024-03-05 15:01:26 -0300

    separate logic createServer to event

--- src/events/src/context_create.js
+++ src/events/src/context_create.js
@@ -1,35 +1,10 @@
-import childProcess from 'child_process';
-import path from 'path';
-import { uuid } from '/utils';
+import { createServer } from '/utils';
 import { context } from '/data';
 import { is, has } from '../test.js';
 
-export const _ = (name, op = {}) => {
+export const _ = (name = '', op = {}) => {
     if (!is.context.validFormat(name)) throw Error('Invalid format to new context');
     if (has.context(name)) throw Error('There is already a context with this name');
 
-    const dir = path.resolve('./dist/server.js');
-    const node = childProcess.fork(dir, {
-        env: {
-            NAME: name,
-        }
-    });
-
-    const requests = {};
-    context.c[name] = {
-        ...op,
-        name,
-        node,
-        send: (exe, data) => new Promise((resolve, reject) => {
-            const id = uuid._();
-            requests[id] = (err, res) => err ? reject(err) : resolve(res);
-            node.send({ exe, id, data });
-        }),
-    };
-
-    node.on('message', (data) => {
-        const { id, res, err } = data;
-        requests[id](err, res);
-        delete requests[id];
-    })
+    context.c[name] = createServer._(name, op);
 }
---
+++ src/utils/src/createServer.js
@@ -0,0 +1,30 @@
+import childProcess from 'child_process';
+import path from 'path';
+import { uuid } from '/utils';
+
+export const _ = (name, op) => {
+    const requests = {};
+    const dir = path.resolve('./dist/server.js');
+    const node = childProcess.fork(dir, {
+        env: {
+            NAME: name,
+        }
+    });
+
+    node.on('message', (data) => {
+        const { id, err, res } = data;
+        requests[id](err, res);
+        delete requests[id];
+    })
+
+    return {
+        ...op,
+        name,
+        node,
+        send: (exe, data) => new Promise((resolve, reject) => {
+            const id = uuid._();
+            requests[id] = (err, res) => err ? reject(err) : resolve(res);
+            node.send({ exe, id, data });
+        }),
+    };
+}
